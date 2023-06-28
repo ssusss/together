c0ea3020152a68590a86f07fa5d4063308c33ca9
kjh425 <k0j4h25@hanmail.net>
2023-06-28 11:40:19 +0900

    Kjh425Servlet 추가!

---
+++ src/main/java/com/together/Kjh425Servlet.java
@@ -0,0 +1,25 @@
+package com.together;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Kjh425Servlet
+ */
+@WebServlet("/kjh425")
+public class Kjh425Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append("Hello, kjh425");
+	}
+
+}
