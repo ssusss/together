1746e956c658d39430c89e2901f35a7306a8f6c0
kjh425 <k0j4h25@hanmail.net>
2023-06-28 14:30:46 +0900

    반갑습니다

--- src/main/java/com/together/AfteryoonServlet.java
+++ src/main/java/com/together/AfteryoonServlet.java
@@ -15,10 +15,15 @@ public class AfteryoonServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	* @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	*/
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.getWriter().append("Hello, afteryoon");
 	}
 
 }
+
+
+
+
+
